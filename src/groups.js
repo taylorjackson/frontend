520330d988f8037c2d759c5a9b14f9e881ed7d30
Hal <73549429+hal0177@users.noreply.github.com>
2023-08-24 18:50:34 +0100

    added socials links

---
+++ src/groups.js
@@ -0,0 +1,60 @@
+const groups = [
+  {
+    title: "Learn about Continuum",
+    links: [
+      {
+        name: "X",
+        url: "https://twitter.com/ContinuumDAO",
+        icon: "x-black.png"
+      },
+      {
+        name: "Medium",
+        url: "https://medium.com/@continuumdao",
+        icon: "medium-black.png"
+      },
+      {
+        name: "GitHub",
+        url: "https://github.com/ContinuumDAO",
+        icon: "github-black.png"
+      }
+    ]
+  },
+  {
+    title: "Join our Community",
+    links: [
+      {
+        name: "Telegram",
+        url: "https://t.me/ContinuumDAO",
+        icon: ""
+      },
+    //   {
+    //     name: "Discord",
+    //     url: "",
+    //     icon: ""
+    //   }
+    ]
+  },
+  {
+    title: "DAO Governance",
+    links: [
+    //   {
+    //     name: "Tally",
+    //     url: "",
+    //     icon: ""
+    //   },
+      {
+        name: "Snapshot",
+        url: "https://snapshot.org/#/continuumdao.eth",
+        icon: ""
+      },
+    //   {
+    //     name: "Coordinape",
+    //     url: "",
+    //     icon: ""
+    //   }
+    ]
+  }
+]
+
+
+export default groups
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,52 @@
 <script>
   import { onMount } from "svelte"
   import { fade, fly } from "svelte/transition"
+  import groups from "../groups"
 
   let ready = false
-  onMount(() => ready = true)
+
+  onMount(() => {
+    ready = true
+  })
+
+  let socials = ""
+
+  for(let i = 0; i < groups.length; i++) {
+    let currentSocialsList = ""
+    const currentGroup = groups[i]
+
+    for(let j = 0; j < groups[i].links.length; j++) {
+      const currentLink = currentGroup.links[j]
+      currentSocialsList = currentSocialsList.concat(`
+        <li><a href="${currentLink.url}" target="_blank">${currentLink.name}</a></li>
+      `)
+    }
+
+    // <img class="link-icon" src="brand-assets/${currentLink.icon}"/>
+
+    socials = socials.concat(`
+      <ul>
+        <h4>${currentGroup.title}</h4> 
+        ${currentSocialsList}
+      </ul>
+    `)
+  }
 </script>
 
 
 {#if ready}
   <header>
-    <img in:fade={{ duration: 1500 }} src="continuum.png" alt="Impossible Triangle"/>
+    <img id="logo" in:fade={{ duration: 1500 }} src="android-chrome-192x192.png" alt="Impossible Triangle"/>
     <h1 in:fly={{ y: 40, duration: 3000 }}>ContinuumDAO</h1>
   </header>
   <main>
-    <a in:fade={{ delay: 1000, duration: 1000 }} href="" target="_blank">White Paper Coming Soon</a>
+    <a in:fade={{ delay: 1000, duration: 1000 }} href="groups.js" download>
+      Download White Paper
+    </a>
   </main>
+  <footer>
+    {@html socials}
+  </footer>
 {/if}
 
 
@@ -23,8 +55,8 @@
     width: 100%;
     margin: 0;
     padding: 0;
-    /*background-color: #fffefe;*/
     background-color: #fffefe;
+    color: #444;
   }
 
   header {
@@ -35,7 +67,7 @@
     width: 100%;
   }
 
-  img {
+  #logo {
     width: 200px;
     height: 200px;
     margin: 20px 0;
@@ -44,10 +76,9 @@
   h1 {
     margin: 0;
     padding: 0;
-    font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
     font-size: 40px;
-    color: #444;
     letter-spacing: 5px;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
   }
 
   main {
@@ -70,5 +101,24 @@
   a:hover {
     background-color: aquamarine;
     transition: 0.5s ease;
+  }
+
+  footer {
+    width: 40%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  }
+
+  :global(li) {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  :global(.link-icon) {
+    width: 25px;
+    height: 25px;
   }
 </style>
